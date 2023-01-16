8b033b142b7e285dac20ac3b934de20a929ce6a3
Anh Ngo <helloanhngo2002@gmail.com>
2023-01-16 14:17:13 -0500

    Finished Dijkstra

--- shortest-path-algo/dijkstra.py
+++ shortest-path-algo/dijkstra.py
@@ -34,6 +34,7 @@ class Graph:
         for i in range(self.v):
             # traverse to the closet node from the current vertex
             min_vertex = self.find_min_vertex()
+            print("Min vertex", min_vertex)
             self.visited[min_vertex] = True
 
             for j in range(self.v):
@@ -48,46 +49,9 @@ class Graph:
                         self.path[j] = self.path[min_vertex] + [j]
 
         for i in range(self.v):
-            print("From", self.start, "to", i, "costs" , self.distance[i], "- Path:", self.path[i])
+            print("From", self.start, "to", i, "costs" , self.distance[i], "\tPath:", self.path[i])
 
         return self.distance
-
-
-
-
-
-
-def dijkstra(adjacency_matrix):
-
-    v = len(adjacency_matrix)
-    visited = [False for i in range(v)]
-    distance = [0 for i in range(v)]
-
-    for i in range(1, v):
-        distance[i] = float('inf')
-
-    for i in range(v-1):
-        min_vertex = find_min_vertex(distance, visited)
-        visited[min_vertex] = True
-
-        for j in range(v):
-            if adjacency_matrix[min_vertex][j] != 0 and not visited[j]:
-                new_dist = distance[min_vertex] + adjacency_matrix[min_vertex][j]
-                if new_dist < distance[j]:
-                    distance[j] = new_dist
-
-    for i in range(v):
-        print(i, " ", distance[i])
-    pass
-
-
-def find_min_vertex(distance, visited):
-
-    min_vertex = -1
-    for i in range(len(distance)):
-        if (min_vertex == -1 or distance[min_vertex] > distance[i]) and not visited[i]:
-            min_vertex = i
-    return min_vertex
 
 
 adjacency_matrix = [
@@ -96,11 +60,9 @@ adjacency_matrix = [
     [5, 0, 0, 0, 0, 4, 0],
     [6, 4, 0, 0, 0, 1, 6],
     [0, 2, 0, 0, 0, 0, 10],
-    [8, 0, 6, 1, 0, 0, 8],
-    [0, 8, 0, 6, 10, 8, 0]
+    [8, 0, 4, 1, 0, 0, 8],
+    [0, 5, 0, 6, 10, 8, 0]
 ]
-
-dijkstra(adjacency_matrix)
 
 g = Graph(adjacency_matrix, 6)
 g.dijkstra()
