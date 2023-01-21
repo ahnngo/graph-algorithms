7fcd5e3d7e08ab29622cb2c3ab947d84a8d4e296
Anh Ngo <helloanhngo2002@gmail.com>
2023-01-20 22:17:44 -0500

    Changing to OOP from functional

--- shortest-path-algo/johnson.py
+++ shortest-path-algo/johnson.py
@@ -1,86 +1,83 @@
 class Graph:
 
-    def __init__(self, vertices):
-        self.v = vertices
-        self.graph = []
+    def __init__(self, graph):
+        self.v = len(graph)
+        self.graph = graph
         self.visited = [False for i in range(self.v)]
         self.dist = [float("inf")] * self.v
+        self.edges = []
+        self.new_graph = [[0 for x in range(self.v)] for y in range(self.v)]
 
+    def add_edge(self):
+        for u in range(self.v):
+            for v in range(self.v):
+                if self.graph[u][v] != 0:
+                    self.edges.append([u, v, self.graph[u][v]])
 
+    def bellman_ford(self):
+        # create an arbitrary source node. The distance to that node is 0
+        self.dist.append(0)
 
-def allPairShortestPath(graph):
-    edges = []
+        # add edges to all the other vertices from arbitrary source
+        arbitrary_edges = 0
+        for i in range(self.v):
+            self.edges.append([self.v, i, 0])
+            arbitrary_edges += 1
 
-    for u in range(len(graph)):
-        for v in range(len(graph)):
-            if graph[u][v] != 0:
-                edges.append([u, v, graph[u][v]])
+        # relax v time (v - 1 if count the arbitrary source)
+        for i in range(self.v):
+            for (u, v, w) in self.edges:
+                # If there is a path from the source to u existing and
+                # the cost from u to v is less than what already exists from source to v
+                # Update the cost and update the path
+                if self.dist[u] != float('inf') and self.dist[u] + w < self.dist[v]:
+                    self.dist[v] = self.dist[u] + w
 
-    distance = bellman_ford(edges, len(graph))
-    # new positive edges weights
-    new_graph = [[0 for x in range(len(graph))] for y in range(len(graph))]
+        # discard the arbitrary source
+        self.dist.pop()
+        for i in range(arbitrary_edges):
+            self.edges.pop()
 
-    for u in range(len(graph)):
-        for v in range(len(graph)):
-            if graph[u][v] != 0:
-                new_graph[u][v] = distance[u] + graph[u][v] - distance[v]
+    def find_min_vertex(self):
+        min_vertex = -1
+        for i in range(self.v):
+            if (min_vertex == -1 or self.dist[min_vertex] > self.dist[i]) and not self.visited[i]:
+                min_vertex = i
+        return min_vertex
 
-    print("New graph: " + str(new_graph))
+    def dijkstra(self, src):
+        # reset visited tracking list and distance
+        self.visited = [False for i in range(self.v)]
+        self.dist = [float("inf")] * self.v
+        self.dist[src] = 0
 
-    # run Dijkstra for every vertex as source
-    for src in range(len(graph)):
-        print("Shortest distance from vertex: ", src)
-        dijkstra(graph, new_graph, src)
+        for i in range(self.v - 1):
+            min_vertex = self.find_min_vertex()
+            self.visited[min_vertex] = True
+            for j in range(self.v):
+                if self.graph[min_vertex][j] != 0 and not self.visited[j]:
+                    new_dist = self.dist[min_vertex] + self.new_graph[min_vertex][j]
+                    if new_dist < self.dist[j]:
+                        self.dist[j] = new_dist
+        for i in range(self.v):
+            print(i, " ", self.dist[i])
 
+    def johnson(self):
+        self.add_edge()
+        self.bellman_ford()
 
-def dijkstra(old_graph, new_graph, src):
-    v = len(new_graph)
-    visited = [False for i in range(v)]
-    distance = [float("inf")] * v
+        # create new graph with positive edges weights
+        for u in range(self.v):
+            for v in range(self.v):
+                if self.graph[u][v] != 0:
+                    self.new_graph[u][v] = self.dist[u] + self.graph[u][v] - self.dist[v]
 
-    distance[src] = 0
+        print("New Graph: ", str(self.new_graph))
 
-    for i in range(v - 1):
-        min_vertex = find_min_vertex(distance, visited)
-        visited[min_vertex] = True
-
-        for j in range(v):
-            if old_graph[min_vertex][j] != 0 and not visited[j]:
-                new_dist = distance[min_vertex] + new_graph[min_vertex][j]
-                if new_dist < distance[j]:
-                    distance[j] = new_dist
-
-    for i in range(v):
-        print(i,  " ", distance[i])
-
-
-def find_min_vertex(distance, visited):
-    min_vertex = -1
-
-    for i in range(len(distance)):
-        if (min_vertex == -1 or distance[min_vertex] > distance[i]) and not visited[i]:
-            min_vertex = i
-
-    return min_vertex
-
-
-def bellman_ford(edges, v):
-    # arbitrary source
-    dist = [float('inf')] * (v + 1)
-    dist[v] = 0
-
-    # add edges to all the other vertices from source
-    for i in range(v):
-        edges.append([v, i, 0])
-
-    # relax v - 1 time
-    for i in range(v):
-        for (u, v, w) in edges:
-            if dist[u] != float('inf') and dist[u] + w < dist[v]:
-                dist[v] = dist[u] + w
-
-    # discard the arbitrary source
-    return dist[:-1]
+        # run Dijkstra for every vertex as source
+        for src in range(self.v):
+            print("Shortest Distance from vertex: ", src)
+            self.dijkstra(src)
 
 
 graph = [[0, 4, 0, 0, 1],
@@ -90,4 +87,5 @@ graph = [[0, 4, 0, 0, 1],
          [0, 0, 0, -5, 0]]
 
 # Johnson's algo
-allPairShortestPath(graph)
+g = Graph(graph)
+g.johnson()
